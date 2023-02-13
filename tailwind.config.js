b20d4e8735d88870487bad7e6c75a5ee18985b00
nehajoshi2003 <nehajoshi252003@gmail.com>
2023-02-13 21:40:44 +0530

    weather app

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('https://miro.medium.com/max/1400/1*zlbzhk8rdFM35PRJLBJ_DQ.jpeg')",
+      
+      }
+    },
+  },
+  plugins: [],
+}
